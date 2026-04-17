835458a56335e1e25054a729c414365c2d548344
Muhammad Haikal Hakim <haikalhakim530@gmail.com>
2026-04-17 22:45:20 +0700

    Update clip-limit.sh

--- hypr/scripts/clip-limit.sh
+++ hypr/scripts/clip-limit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 pkill wl-paste
 
-# Teks: Simpan, terus hapus ID ke-11 dan seterusnya
 wl-paste --type text --watch bash -c "cliphist store && cliphist list | tail -n +11 | cut -f1 | cliphist delete" &
-
-# Gambar: Sama, biar screenshot F12 lu juga kena limit
 wl-paste --type image --watch bash -c "cliphist store && cliphist list | tail -n +11 | cut -f1 | cliphist delete"
